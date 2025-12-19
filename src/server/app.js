cdf26c54a975d1f1d9e13723200e32dca0d976ac
filinov <filinov.victor@yandex.ru>
2025-12-19 21:21:16 +0500

    feat: добавить middleware для логирования входящих запросов.

--- src/server/app.js
+++ src/server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import { attachAdminPanel } from "../dashboard/panel.js";
 import env from "../config/env.js";
+import logger from "../utils/logger.js";
 
 /**
  * Creates and configures an Express application.
@@ -9,6 +10,17 @@ import env from "../config/env.js";
  */
 export const createApp = () => {
     const app = express();
+
+    // Request logging middleware
+    app.use((req, res, next) => {
+        logger.info({
+            method: req.method,
+            url: req.url,
+            headers: req.headers,
+            body: req.body // Body might be empty here if processed later, but express.json() is below
+        }, "Incoming request");
+        next();
+    });
 
     app.use(
         helmet({
